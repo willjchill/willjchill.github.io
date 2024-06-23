e66e9a5a4de79069fb7b04ee90516f19e1def2f8
supercaptinpanda <160202721+supercaptinpanda@users.noreply.github.com>
2024-06-23 18:00:15 -0400

    dropdown fix

--- menu_dropdown.js
+++ menu_dropdown.js
@@ -1,5 +1,4 @@
 document.querySelector('#hamburger-card').addEventListener('click', function() {
-    console.log("hello!");
     const menuPage = document.querySelector('#menu-page');
     if (menuPage.style.top === '0%') {
         menuPage.style.top = '-100%';
