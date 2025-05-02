e6d376ee5b292000fff35715cbf0f4d5293b74ac
William Desueza <willjdesueza@gmail.com>
2025-05-02 12:40:20 -0700

    compressing images

--- menu_dropdown.js
+++
@@ -1,8 +0,0 @@
-document.querySelector('#hamburger-card').addEventListener('click', function() {
-    const menuPage = document.querySelector('#menu-page');
-    if (menuPage.style.top === '0%') {
-        menuPage.style.top = '-100%';
-    } else {
-        menuPage.style.top = '0%';
-    }
-});
